c8dc992cf97eea306bac7eb02421b758262098c9
crisber99 <bcbs1986@gmail.com>
2024-11-02 02:16:18 -0300

    avance

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function hola(){
-    
+
+}
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Genial, activaste este mensaje de alerta.', 'success')
+  })
 }
